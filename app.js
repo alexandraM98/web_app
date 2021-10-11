66d395a7f5cc18dc877959da71806a19e170338d
rainstorm <alexandra_mitu98@yahoo.com>
2021-10-11 20:54:54 +0100

    localhost

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 
 
 /* Setting the app port */
-app.set("port", process.env.port || 3000);
+app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"),function(){
   console.log("Server started on port " + app.get("port"));
 
